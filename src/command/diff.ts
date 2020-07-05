72df331043a715f147a3d6f0b84806f2055166f0
r-tamura <rtamura30@gmail.com>
2020-07-05 16:19:39 +0900

    print_diffへ移行漏れコードの移行

--- src/command/diff.ts
+++ src/command/diff.ts
@@ -1,13 +1,11 @@
-import * as path from "path";
 import { Base } from "./base";
 import * as Repository from "../repository";
-import { Pathname, OID } from "../types";
+import { Pathname } from "../types";
 import * as Database from "../database";
 import * as arg from "arg";
 import * as Index from "../gindex";
 import { asserts } from "../util";
-import { diff, Hunk, TextDocument, Edit } from "../diff";
-import { definePrintDiffOptions, Target, NULL_OID } from "./shared/print_diff";
+import { definePrintDiffOptions, Target, printDiff, NULL_OID } from "./shared/print_diff";
 import { Stage } from "../gindex";
 
 interface Option {
@@ -67,17 +65,17 @@ export class Diff extends Base<Option> {
         case "added": {
           const targetFromIndex = await this.fromIndex(pathname);
           asserts(targetFromIndex !== null, `ファイル '${pathname}' は存在する`);
-          this.printDiff(this.fromNothing(pathname), targetFromIndex);
+          printDiff(this.fromNothing(pathname), targetFromIndex, this);
           break;
         }
         case "modified": {
           const targetFromIndex = await this.fromIndex(pathname);
           asserts(targetFromIndex !== null, `ファイル '${pathname}' は存在する`);
-          this.printDiff(await this.fromHead(pathname), targetFromIndex);
+          printDiff(await this.fromHead(pathname), targetFromIndex, this);
           break;
         }
         case "deleted": {
-          this.printDiff(await this.fromHead(pathname), this.fromNothing(pathname));
+          printDiff(await this.fromHead(pathname), this.fromNothing(pathname), this);
           break;
         }
       }
@@ -98,10 +96,6 @@ export class Diff extends Base<Option> {
         await this.printWorkspaceDiff(pathname);
       }
     }
-  }
-
-  private diffHunks(a: TextDocument, b: TextDocument) {
-    return Hunk.filter(diff(a, b));
   }
 
   private async fromHead(pathname: Pathname) {
@@ -133,22 +127,6 @@ export class Diff extends Base<Option> {
     return Target.of(pathname, NULL_OID, null, "");
   }
 
-  private header(text: string) {
-    this.log(this.fmt("bold", text));
-  }
-
-  private printDiff(a: Target, b: Target) {
-    if (a.equals(b)) {
-      return;
-    }
-
-    a.name = path.join("a", a.name);
-    b.name = path.join("b", b.name);
-    this.log(`diff --git ${a.name} ${b.name}`);
-    this.printMode(a, b);
-    this.printDiffContent(a, b);
-  }
-
   private async printConflictDiff(pathname: Pathname) {
     this.log(`* Unmerged path ${pathname}`);
 
@@ -156,7 +134,7 @@ export class Diff extends Base<Option> {
     if (!target) {
       return;
     }
-    this.printDiff(target, await this.fromFile(pathname));
+    printDiff(target, await this.fromFile(pathname), this);
   }
 
   private async printWorkspaceDiff(pathname: Pathname) {
@@ -164,71 +142,15 @@ export class Diff extends Base<Option> {
       case "modified": {
         const targetFromIndex = await this.fromIndex(pathname);
         asserts(targetFromIndex !== null, `ファイル '${pathname}' は存在する`);
-        this.printDiff(targetFromIndex, await this.fromFile(pathname));
+        printDiff(targetFromIndex, await this.fromFile(pathname), this);
         break;
       }
       case "deleted": {
         const targetFromIndex = await this.fromIndex(pathname);
         asserts(targetFromIndex !== null, `ファイル '${pathname}' は存在する`);
-        this.printDiff(targetFromIndex, this.fromNothing(pathname));
+        printDiff(targetFromIndex, this.fromNothing(pathname), this);
         break;
       }
     }
-  }
-
-  private printMode(a: Target, b: Target) {
-    if (a.mode === null) {
-      this.header(`new file mode ${b.mode}`);
-    } else if (b.mode === null) {
-      this.header(`deleted file mode ${a.mode}`);
-    } else if (a.mode !== b.mode) {
-      this.header(`old mode ${a.mode}`);
-      this.header(`new mode ${b.mode}`);
-    }
-  }
-
-  private printDiffContent(a: Target, b: Target) {
-    if (a.equalsContent(b)) {
-      return;
-    }
-
-    let oidRange = `index ${this.short(a.oid)}..${this.short(b.oid)}`;
-    if (a.mode === b.mode) {
-      oidRange += ` ${a.mode}`;
-    }
-    this.log(oidRange);
-    this.log(`--- ${a.deffPath}`);
-    this.log(`+++ ${b.deffPath}`);
-
-    const hunks = this.diffHunks(a.data, b.data);
-    hunks.forEach(this.printDiffHunk.bind(this));
-  }
-
-  private printDiffEdit(edit: Edit) {
-    const text = edit.toString();
-    switch (edit.type) {
-      case "eql":
-        this.log(text);
-        break;
-      case "ins":
-        this.log(this.fmt("green", text));
-        break;
-      case "del":
-        this.log(this.fmt("red", text));
-        break;
-      default:
-        throw TypeError(`diff: invalid type '${edit.type}'`);
-    }
-  }
-
-  private printDiffHunk(hunk: Hunk) {
-    this.log(this.fmt("cyan", hunk.header()));
-    hunk.edits.forEach((e) => {
-      this.printDiffEdit(e);
-    });
-  }
-
-  private short(oid: OID) {
-    return this.repo.database.shortOid(oid);
   }
 }
